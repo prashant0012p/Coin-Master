63ad2e60c79157c667830ac528199c4cd2fc71fe
Tejas Suresh Vispute <tejasvispute25@gmail.com>
2022-10-01 13:22:13 +0530

    changes added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var box1 = document.getElementById('one');
 var box2 = document.getElementById('two');
 var box3 = document.getElementById('three');
 
+var randomNumber1;
+var randomNumber2;
+var randomNumber3;
 
 // container
 
@@ -16,47 +19,39 @@ Button.setAttribute("onclick", "clickFunc()");
 
 
 
-var randomNumber1 = Math.floor(Math.random() * 6);
-var randomNumber2 = Math.floor(Math.random() * 6);
-var randomNumber3 = Math.floor(Math.random() * 6);
 
 
 
-let random = [randomNumber1, randomNumber2, randomNumber3];
-console.log(random);
 
 
 
 function clickFunc() {
 
+    let randomemoji1 = ["😀", "😂", "😉", "🙂", "😍"];
+    let randomemoji2 = ["😀", "😂", "😉", "🙂", "😍"];
+    let randomemoji3 = ["😀", "😂", "😉", "🙂", "😍"];
+
+    randomNumber1 = Math.floor(Math.random() * 5);
+    randomNumber2 = Math.floor(Math.random() * 5);
+    randomNumber3 = Math.floor(Math.random() * 5);
 
     setTimeout(() => {
+        console.log(randomNumber1, randomNumber2, randomNumber3);
 
-        box1.innerHTML = random[0];
-        box2.innerHTML = random[1];
-        box3.innerHTML = random[2];
-
-
-    }, 3000);
-
-    if (random[0] == random[1] && random[2] == random[1]) {
-        box1.innerHTML = random[0];
-        box2.innerHTML = random[1];
-        box3.innerHTML = random[2];
-    } else {
-        reload();
-    }
+        box1.innerHTML = randomemoji1[randomNumber1];
+        box2.innerHTML = randomemoji2[randomNumber2];
+        box3.innerHTML = randomemoji3[randomNumber3];
 
 
-
+    }, 1000);
 
 }
 
 
-function reload() {
+// function reload() {
 
-    setTimeout(() => {
-        window.location.reload(true);
-    }, 6000);
+//     setTimeout(() => {
+//         window.location.reload(true);
+//     }, 6000);
 
-}
+// }
